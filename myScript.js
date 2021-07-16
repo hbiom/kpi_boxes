de1563f14fdf95c3112a1536ef955c29bb9da8da
Hugo Bottois <bottois.hugo@outlook.fr>
2021-07-16 13:57:04 +0200

    typo

--- myScript.js
+++ myScript.js
@@ -2,17 +2,21 @@ function animateValue(start, duration) {
 
   let startTimestamp = null;
   var kpiValue = document.querySelectorAll('span.kpi-value');
+
   kpiValue.forEach(element=> {
     let end = parseInt(element.innerHTML)
-    let step = (timestamp) => {
+    let steps = (timestamp) => {
       if (!startTimestamp) startTimestamp = timestamp;
+
       const progress = Math.min((timestamp - startTimestamp) / duration, 1);
       element.innerHTML = Math.floor(progress * (end - start) + start);
+      console.log(progress * (end - start))
+
       if (progress < 1) {
-        window.requestAnimationFrame(step);
+        window.requestAnimationFrame(steps);
       }
     };
-  window.requestAnimationFrame(step);
+  window.requestAnimationFrame(steps);
   })
 };
 
